185e18f29b4a2e7a3149b221d6d8027546a96755
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-07-13 19:10:36 +0200

    Setup method for collectables added. Working, not final.

--- Source/Simple_Platformer/PlatformerGameMode.cpp
+++ Source/Simple_Platformer/PlatformerGameMode.cpp
@@ -2,4 +2,35 @@
 
 
 #include "PlatformerGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
+// ! PROTOTYPE
+// Get every platform. Used to setup special platforms' movement.
+void APlatformerGameMode::GetPlatforms() 
+{
+    TArray<AActor*> Platforms;
+    UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("Platform")), Platforms);
+
+    for(AActor* Platform : Platforms){
+        if(Platform -> ActorHasTag("Vertical")){
+            UE_LOG(LogTemp, Warning, TEXT("Piattaforma verticale"));
+        } else if(Platform -> ActorHasTag("Horizontal")){
+            UE_LOG(LogTemp, Warning, TEXT("Piattaforma orizzontale"));
+        } else {
+            UE_LOG(LogTemp, Warning, TEXT("Piattaforma statica"));
+        }
+    }
+}
+
+// Get the number of collectable items. Called by player HUD.
+int32 APlatformerGameMode::GetTotalCollectables() 
+{
+    TArray<AActor*> TotalCollectables;
+    UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("Collectible")), TotalCollectables);
+    return TotalCollectables.Num();
+}
+
+// void APlatformerGameMode::SetupHUDVariables() 
+// {
+    
+// }
--- Source/Simple_Platformer/PlatformerGameMode.h
+++ Source/Simple_Platformer/PlatformerGameMode.h
@@ -13,5 +13,29 @@ UCLASS()
 class SIMPLE_PLATFORMER_API APlatformerGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void GetPlatforms();
+
+	// Collectables
+		UFUNCTION(BlueprintCallable)
+			int32 GetTotalCollectables();
+
+		// UFUNCTION(BlueprintCallable)
+		// 	void SetupHUDVariables();
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collectable")
+			int32 TotalItems;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collectable")
+			int32 CalculatedTotal;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collectable")
+			bool IsCalculated;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collectable")
+			int32 ObtainedItems;
+	//
 };
